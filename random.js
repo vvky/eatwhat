5b4617e168a39400bef2608e96d20659265c788f
wangkaiyu <vvkyjay@163.com>
2024-04-19 14:57:24 +0800

    eatwhat

--- random.js
+++ random.js
@@ -2,17 +2,7 @@ $(function () {
     var run = 0,
         heading = $("h1"),
         timer;
-    console.log(
-        "该工具用来解决「吃啥」这一被人类公认排在人生 N 大难题前列的问题。\n" +
-        "\n" +
-        "简单，小巧，能够迅速帮助人们解决苦恼，消除纠纷。\n" +
-        "\n" +
-        "无数个日子里，无数个高矮胖瘦男女老少各不相同的个体每天都会思考的问题。\n" +
-        "\n" +
-        "「吃啥」，从现在起，就要被改变。\n" +
-        "\n" +
-        "还有一周就口蘑口蘑 🙏"
-    )
+    console.log("还有一周就口蘑口蘑 🙏")
     $("#start").click(function () {
         var list = $("#list").val().replace(/ +/g, " ").replace(/^ | $/g, "").split(" ");
         if (!run) {
